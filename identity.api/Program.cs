034a29486450a5878d97843133d58f381658caf7
Loc Nguyen <loc.nguyen@vklink.vn>
2023-06-30 16:07:50 +0700

    add dockerfile (tested)

--- identity.api/Program.cs
+++ identity.api/Program.cs
@@ -104,11 +104,9 @@ builder.Services.AddSwaggerGen(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
